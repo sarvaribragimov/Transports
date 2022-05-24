87f4d9680611011f734aa5010ef54a356781fd44
sarvaribragimov <92175845+sarvaribragimov@users.noreply.github.com>
2022-05-24 20:02:27 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from pyexpat import model
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+#title
+st.title("Rasmlarni klassifikatsiya qiluvchi model (Boat,Airplane,Car)  Muallif: Ibragimov Sarvar ")
+# Rasmni yuklash
+file = st.file_uploader("Rasmni yuklash",type=['png','jpg','jpeg',"gif",'svg'])
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+    model = load_learner('image_predict.pkl')
+# Bashorat
+
+    pred, pred_id, probs = model.predict(img)
+
+    st.success(f'Bashorat: {pred}')
+    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
